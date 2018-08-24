b028e93108b137bca107a67e543a358ab36c1edc
Edy Luna <eluna060499@gmail.com>
2018-08-24 13:28:57 -0500

    Initial commit

---
+++ Module01GettingStarted/Module01GettingStarted/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Edy Luna
+// Module01GettingStarted
+// 08-24-18
+
+namespace Module01GettingStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
